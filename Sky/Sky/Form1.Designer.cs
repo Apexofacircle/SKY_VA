e4a731b70325bca76cf7f8db13f4d9c28655a681
Apexofacircle <96547482+Apexofacircle@users.noreply.github.com>
2022-04-21 12:48:16 -0400

    update

--- Sky/Sky/Form1.Designer.cs
+++ Sky/Sky/Form1.Designer.cs
@@ -33,19 +33,21 @@
             // 
             // TextRecognized
             // 
-            this.TextRecognized.Location = new System.Drawing.Point(265, 125);
+            this.TextRecognized.Location = new System.Drawing.Point(208, 142);
+            this.TextRecognized.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TextRecognized.Multiline = true;
             this.TextRecognized.Name = "TextRecognized";
-            this.TextRecognized.Size = new System.Drawing.Size(272, 26);
+            this.TextRecognized.Size = new System.Drawing.Size(242, 22);
             this.TextRecognized.TabIndex = 0;
             this.TextRecognized.TextChanged += new System.EventHandler(this.TextRecognized_TextChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(711, 360);
             this.Controls.Add(this.TextRecognized);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- Sky/Sky/Form1.cs
+++ Sky/Sky/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Sorry for spelling
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,7 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//VA recognition and output
 using System.Speech.Recognition;
 using System.Speech.Synthesis;
 
@@ -22,46 +25,35 @@ namespace Sky
             InitializeComponent();
         }
 
-        //bool Active = true;
+        //TD: make recognition more acurate.
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // speach to text
+            // speech to text
             SpeechRecognitionEngine recognizer =
              new SpeechRecognitionEngine(
-             new System.Globalization.CultureInfo("en-US"));
-
+                 new System.Globalization.CultureInfo("en-US"));
             recognizer.LoadGrammar(new DictationGrammar());
-
             recognizer.SpeechRecognized +=
-          new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
-
+                new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
             recognizer.SetInputToDefaultAudioDevice();
-
             recognizer.RecognizeAsync(RecognizeMode.Multiple);
-
-            
         }
 
         // Handle the SpeechRecognized event.  
         void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
 
-
+            //speech syntisis
             TextRecognized.Text = e.Result.Text;
-
             var synthesizer = new SpeechSynthesizer();
             synthesizer.SetOutputToDefaultAudioDevice();
 
-            
-                synthesizer.Speak(e.Result.Text);
-            
-            
+            synthesizer.Speak(e.Result.Text);
         }
 
         private void TextRecognized_TextChanged(object sender, EventArgs e)
         {
-
         }
     }
 }
