757e6077923e7278ccd7b76981683f6b4698d4ea
Apexofacircle <96547482+Apexofacircle@users.noreply.github.com>
2022-04-21 12:23:27 -0400

    first update

---
+++ Sky/Sky/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Sky
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextRecognized = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TextRecognized
+            // 
+            this.TextRecognized.Location = new System.Drawing.Point(265, 125);
+            this.TextRecognized.Multiline = true;
+            this.TextRecognized.Name = "TextRecognized";
+            this.TextRecognized.Size = new System.Drawing.Size(272, 26);
+            this.TextRecognized.TabIndex = 0;
+            this.TextRecognized.TextChanged += new System.EventHandler(this.TextRecognized_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TextRecognized);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextRecognized;
+    }
+}
+
---
+++ Sky/Sky/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+
+
+namespace Sky
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //bool Active = true;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // speach to text
+            SpeechRecognitionEngine recognizer =
+             new SpeechRecognitionEngine(
+             new System.Globalization.CultureInfo("en-US"));
+
+            recognizer.LoadGrammar(new DictationGrammar());
+
+            recognizer.SpeechRecognized +=
+          new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+
+            recognizer.SetInputToDefaultAudioDevice();
+
+            recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+            
+        }
+
+        // Handle the SpeechRecognized event.  
+        void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+
+
+            TextRecognized.Text = e.Result.Text;
+
+            var synthesizer = new SpeechSynthesizer();
+            synthesizer.SetOutputToDefaultAudioDevice();
+
+            
+                synthesizer.Speak(e.Result.Text);
+            
+            
+        }
+
+        private void TextRecognized_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
